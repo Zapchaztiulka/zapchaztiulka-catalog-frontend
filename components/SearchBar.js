c921b3245ec13924c4cf68e3c12d7831fe108031
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-09-11 18:30:00 +0300

    Added hover for links in footer

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -69,14 +69,8 @@ const SearchBar=() =>{
               placeholder="Я шукаю.."
               className="search-input"
             />
-            <div className="search-icon">
-              <Image
-                src="/icons/search-icon.svg"
-                alt="search-icon"
-                className="object-contain"
-                width={24}
-                height={24}
-              />
+            <div className="search-icon">               
+                <svg className="stroke-secondary icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M11.25 19.5a8.25 8.25 0 1 0 0-16.5 8.25 8.25 0 0 0 0 16.5ZM21 21l-3.75-3.75"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
             </div>
 </div>          
             <Transition
