f1b7cfdb731785b8c241962bccfb6afde0d71a89
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-10-06 11:37:39 +0300

    Clear some code

--- pages/index.js
+++ pages/index.js
@@ -5,36 +5,8 @@ import CardsList from "@/components/CardsList";
 import { useSelector } from "react-redux";
 import { selectProducts } from "@/redux/products/productsSelectors";
 
-// export async function getServerSideProps() {
-//   try {
-//     const response = await fetch('http://localhoshttps://spares-backend-i2mq.onrender.com/api/products');
-//     if (!response.ok) {
-//       throw new Error('Failed to fetch data');
-//     }
-//     const products = await response.json();
-//     return {
-//       props: {
-//         products,
-//       },
-//     };
-//   } catch (error) {
-//     console.error('Error fetching products:', error);
-//     return {
-//       props: {
-//         products: [],
-//       },
-//     };
-//   }
-// }
-
-
-
 
 const Catalog = ({products}) => {
-  // const { isLoading, isFetching, error } = useGetProductsBySearchQuery("");
-
-  // const products = useSelector(selectProducts)
-  // console.log(products)
 
   return (
     <div className="mt-[130px] flex gap-5">
