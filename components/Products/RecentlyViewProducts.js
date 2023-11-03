a5ae37058868b7c6c4c15b84c72ef0d1d0984445
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-11-03 21:35:00 +0300

    Fixed localstorage for viewed products

--- components/Products/RecentlyViewProducts.js
+++ components/Products/RecentlyViewProducts.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
-const RecentlyViewProducts = ({viewProduct}) => {
+const RecentlyViewProducts = ({productFromLocalStorage}) => {
   return (
     <>
       <div>
         <ul className="flex flex-wrap gap-[7px] tablet600:gap-xs tablet1024:gap-s desktop1200:gap-sPlus justify-center mb-5">
-              {viewProduct && viewProduct.map((item, index) => {
-                 return (
-                    <li key={`${item._id}+${index}`}>{item.name}</li>
-                 )
-              }                
-          )}
+          {productFromLocalStorage &&
+            productFromLocalStorage?.map((item, index) => {
+              return <li key={`${item._id}+${index}`}>{item.name}</li>;
+            })}
         </ul>
       </div>
     </>
