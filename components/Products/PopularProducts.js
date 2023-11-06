1976480c5898254146ed16050ba3cd9b018a229f
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-11-06 13:11:48 +0300

    Added markup for popular products

---
+++ components/Products/PopularProducts.js
@@ -0,0 +1,87 @@
+import React from "react";
+import Loader from "../Loader";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectIsLoading,
+  selectProducts,
+} from "@/redux/products/productsSelectors";
+import { cutArticle, cutProductsArray, cutText } from "@/helpers/cutTiext";
+import Image from "next/image";
+import Link from "next/link";
+
+const PopularProducts = () => {
+  const dispatch = useDispatch();
+  const data = useSelector(selectProducts);
+  const products = data?.products;
+  const isLoading = useSelector(selectIsLoading);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+   // cutProductsArray(products)
+
+   console.log(cutProductsArray(products))
+
+  return (
+    <>
+      {isLoading && data.length === 0 && <Loader />}
+      <div>
+        <ul className="flex flex-wrap gap-[7px] tablet600:gap-xs tablet1024:gap-s desktop1200:gap-sPlus justify-center mb-5">
+          {products?.map(({ name, _id, photo, price, vendorCode }) => {
+            return (
+              <li className="product-card" key={_id}>
+                <Link href={{ pathname: `/product/${id}` }}>
+                  <div className="">
+                    <div className="w-full">
+                      {photo.length === 0 || !getExtension(photo[0]?.url) ? (
+                        <Image
+                          src="/empty-img.jpeg"
+                          alt="no image"
+                          width="0"
+                          height="0"
+                          sizes="100vw"
+                          className="product-card-img "
+                        />
+                      ) : (
+                        <Image
+                          src={photo[0]?.url}
+                          alt={photo[0]?.alt}
+                          width="0"
+                          height="0"
+                          priority
+                          sizes="100vw"
+                          className="product-card-img "
+                        />
+                      )}
+                    </div>
+
+                    <div className="flex flex-col grow tablet768:px-3 tablet768:pt-2 tablet768:pb-3 p-2">
+                      <p className="mb-1 tablet768:text-sm text-[10px] text-textTertiary">
+                        Артикул: {cutArticle(vendorCode)}
+                      </p>
+                      <h3 className="mobile320:mb-xs tablet600:mb-sPlus desktop1200:mb-m2 overflow-y-hidden mobile320:h-[58px] tablet600:h-[70px] tablet1024:h-[78px] desktop1200:h-[78px] mobile320:text-sm tablet600:text-base desktop1200:text-lg text-textPrimary">
+                        {cutText(name)}
+                      </h3>
+                      <p className="tablet768:mb-2 mb-1 font-medium text-textPrimary tablet768:text-2xl text-lg">
+                        {price.value} &#8372;
+                      </p>
+
+                      <div>
+                        <button className=" tablet768:px-6 tablet768:py-3 py-2 w-full text-textContrast tablet768:text-base text-sm tablet768:font-medium state-button ">
+                          Додати в кошик
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default PopularProducts;
--- helpers/cutTiext.js
+++ helpers/cutTiext.js
@@ -11,3 +11,14 @@ export function cutArticle(string) {
     ? string
     : string.slice(0, quantityChar) + '...';
 }
+
+export function cutProductsArray(products) {
+  switch (window.innerWidth) {
+    case window.innerWidth >= 320:
+      return products.slice(0, 4);
+    case window.innerWidth >= 1024:
+      return products.slice(0, 3);
+    case window.innerWidth >= 1920:
+      return products.slice(0, 4);
+ }
+}
