97888d3a24c416099c4f826f45a29d4f3cd9c14b
Oleh Kliapko <goldclub1977@gmail.com>
2023-11-21 10:42:03 +0200

    FIX: chat height

--- components/Chat/Chat.js
+++ components/Chat/Chat.js
@@ -1,49 +1,48 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import { socket } from "./socket";
-import { CloseIcon } from "@/public/icons";
+import { socket } from './socket';
+import { CloseIcon } from '@/public/icons';
 
 const BASE_URL =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === 'development'
     ? process.env.BASE_DEV_URL
     : process.env.BASE_PROD_URL;
 
 export const Chat = ({ onClick }) => {
   const [isOpenChat, setIsOpenChat] = useState(true);
-  const storedUserId = localStorage.getItem("userId");
+  const storedUserId = localStorage.getItem('userId');
 
   useEffect(() => {
-    socket.on("minimizeChat", ({ userId, isChatRoomOpen }) => {
+    socket.on('minimizeChat', ({ userId, isChatRoomOpen }) => {
       if (storedUserId === userId) {
         setIsOpenChat(isChatRoomOpen === undefined ? true : isChatRoomOpen);
       }
     });
 
     return () => {
-      socket.off("minimizeChat");
+      socket.off('minimizeChat');
     };
   }, [storedUserId]);
 
   return (
     <div
-      className={`sticky bottom-0 left-1/2 z-[100] ${
-        isOpenChat ? "w-[50vw] h-[70vh]" : "w-[50vw] h-[7vh]"
-      } bg-bgWhite border-2 border-solid border-borderDefault shadow-xl`}
+      className={`sticky bottom-0 left-1/2 z-[100] bg-bgWhite border-2 border-solid
+                border-borderDefault shadow-xl transition-all duration-300 ease-in-out ${
+                  isOpenChat ? 'w-[50vw] h-[70vh]' : 'w-[50vw] h-[7vh]'
+                }`}
     >
-      <div className=" bg-bgBrandDark">
-        <button
-          className="close-chat-btn"
-          type="button"
-          onClick={() => onClick()}
-        >
-          <CloseIcon
-            className="close-icon stroke-iconWhite"
-            width="44"
-            height="44"
-            strokeWidth="2"
-          />
-        </button>
-      </div>
+      <button
+        className="close-chat-btn"
+        type="button"
+        onClick={() => onClick()}
+      >
+        <CloseIcon
+          className="close-icon stroke-iconWhite"
+          width="44"
+          height="44"
+          strokeWidth="2"
+        />
+      </button>
       <iframe
         src={`${BASE_URL}?userId=${storedUserId}`}
         className="w-full h-full"
