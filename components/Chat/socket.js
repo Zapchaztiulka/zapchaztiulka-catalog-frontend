8ae4fc80a101d32fee0b96e7990a37f50e0a639a
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-11-24 14:47:52 +0300

    Changed name env in socket back

--- components/Chat/socket.js
+++ components/Chat/socket.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 const BACKEND_URL =
   process.env.NODE_ENV === 'development'
-    ? process.env.BASE_DEV_URL
+    ? process.env.BACKEND_DEV_URL
     : process.env.BACKEND_PROD_URL;
 
 export const socket = io.connect(BACKEND_URL);
