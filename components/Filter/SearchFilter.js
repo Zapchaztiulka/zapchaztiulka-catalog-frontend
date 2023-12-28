df460ede424d49545d04409b00fe29b984e3706f
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-12-28 19:14:35 +0300

    Fixed css for search filter

--- components/Filter/SearchFilter.js
+++ components/Filter/SearchFilter.js
@@ -1,7 +1,6 @@
-
 import theme from '@/presets';
 import { CloseIcon } from 'universal-components-frontend/src/components/icons';
-import {SearchIcon} from 'universal-components-frontend/src/components/icons';
+import { SearchIcon } from 'universal-components-frontend/src/components/icons';
 
 const SearchFilter = ({
   handleSearch,
@@ -10,29 +9,32 @@ const SearchFilter = ({
   placeholderName,
 }) => {
   return (
-    <div    
-      className="filter mb-2 flex items-center gap-[10px] tablet600:w-[343px] tablet1024:w-full py-xs px-xs border rounded-minimal border-borderDefault"
-    >
-      <SearchIcon color={theme.extend.colors.iconSecondary} />
-      <div className="flex justify-between w-full ">
-              <input
-        className="text-base/[24px] placeholder:text-textInputDefault text-textPrimary focus:outline-none"
-        placeholder={placeholderName}
-        value={value}
-        onChange={handleSearch}
-      />
-      {value !== '' && (
-        <button className="close-btn" type="button" onClick={removeSearchTerm}>
-          <CloseIcon
-              color={theme.extend.colors.iconBrand}
-              size="20"
-          />
-        </button>
-      )}
-      </div>
+    <div className="filter mb-2 flex items-center gap-2 tablet600:w-[343px] tablet1024:w-full py-xs px-xs border rounded-minimal border-borderDefault">
+      <div className="flex items-center w-full gap-[10px]">
+        <div>
+          <SearchIcon color={theme.extend.colors.iconSecondary} />
+        </div>
 
+        <div className=" relative w-full">
+          <input
+            className="flex-grow w-full text-base placeholder:text-textInputDefault text-textPrimary focus:outline-none"
+            placeholder={placeholderName}
+            value={value}
+            onChange={handleSearch}
+          />
+          {value !== '' && (
+            <button
+              className=" absolute right-0 top-0"
+              type="button"
+              onClick={removeSearchTerm}
+            >
+              <CloseIcon color={theme.extend.colors.iconBrand} size="20" />
+            </button>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default SearchFilter
+export default SearchFilter;
