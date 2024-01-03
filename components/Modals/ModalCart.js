bfb3db3d00a496610826f5c31d769274e1f24296
Midland707 <rus14job@ukr.net>
2024-01-03 23:20:41 +0200

    add css styles to label "Кошик"

--- components/Modals/ModalCart.js
+++ components/Modals/ModalCart.js
@@ -10,14 +10,23 @@ const ModalCart = ({ onClose }) => {
   return (
     <Modal onClose={onClose}>
       <div
-        className="flex flex-wrap content-between px-[16px] py-[24px] w-screen
-      mobile320:h-[424px] mobile375:h-[756px] mobile480:h-[876px]
-       tablet600:h-[904px] tablet768:h-[1077px] tablet1024:w-[976px] tablet1024:h-[546px]
-       desktop1440:w-[976px] desktop1440:w-[780px]"
+        className="flex flex-wrap content-between w-screen 
+        mobile320:p-[16px] mobile480:px-[24px] mobile480:py-[16px] tablet600:px-[24px] tablet600:py-[20px]
+        tablet1024:px-[48px] desktop1440:py-[40px]
+        mobile320:h-[424px] mobile375:h-[756px] mobile480:h-[876px]
+        tablet600:h-[904px] tablet768:h-[1077px] tablet1024:w-[976px] tablet1024:h-[546px] desktop1440:h-[780px]"
         //  mobile320:w-[320px] mobile375:w-[357px] mobile480:w-[480px] tablet600:w-[600px] tablet768:w-[768px]"
       >
-        <div className="flex flex-row items-center gap-[8px]">
-          <h5>Кошик</h5>
+        <div
+          className="flex mobile320:flex-row tablet600:justify-between 
+        items-center mobile320:gap-[8px] w-full pr-[30px]"
+        >
+          <h5
+            className="decoration-textPrimary mobile320:font-medium mobile320:text-[18px] mobile320:leading-[25.2px]
+           tablet600:text-[28px] tablet600:leading-[33.6px] desktop1440:font-normal desktop1440:text-[36px] desktop1440:leading-[46.8px]"
+          >
+            Кошик
+          </h5>
           <button className="w-[152px] h-[40px]" type="button">
             Очистити кошик
           </button>
