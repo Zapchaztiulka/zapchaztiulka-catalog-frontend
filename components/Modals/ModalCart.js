707aab731042797e0aede5cf991c5aa6dc015f86
Midland707 <rus14job@ukr.net>
2024-01-04 23:32:41 +0200

    add css styles to cart components

--- components/Modals/ModalCart.js
+++ components/Modals/ModalCart.js
@@ -10,15 +10,14 @@ const ModalCart = ({ onClose }) => {
   return (
     <Modal onClose={onClose}>
       <div
-        className="flex flex-wrap content-between w-screen 
+        className="w-screen flex flex-col
         mobile320:p-[16px] mobile480:px-[24px] mobile480:py-[16px] tablet600:px-[24px] tablet600:py-[20px]
-        tablet1024:px-[48px] desktop1440:py-[40px]
-        mobile320:h-[424px] mobile375:h-[756px] mobile480:h-[876px]
-        tablet600:h-[904px] tablet768:h-[1077px] tablet1024:w-[976px] tablet1024:h-[546px] desktop1440:h-[780px]"
-        //  mobile320:w-[320px] mobile375:w-[357px] mobile480:w-[480px] tablet600:w-[600px] tablet768:w-[768px]"
+        tablet1024:px-[48px] desktop1440:py-[40px] tablet1024:w-[976px]
+        mobile320:h-[424px] mobile375:h-[756px] mobile480:h-[876px] tablet600:h-[904px]
+        tablet768:h-[1077px] tablet1024:h-[546px] desktop1440:h-[780px]"
       >
         <div
-          className="flex mobile320:flex-row tablet600:justify-between 
+          className="self-start flex mobile320:flex-row tablet600:justify-between 
         items-center mobile320:gap-[8px] w-full pr-[30px]"
         >
           <h5
@@ -35,17 +34,21 @@ const ModalCart = ({ onClose }) => {
             Очистити кошик
           </button>
         </div>
-        <div className="flex flex-col items-center justify-end w-screen">
+        <div className="mt-auto mb-auto flex flex-col items-center w-full">
           <div className="flex items-center justify-center mb-[8px] w-[59px] h-[59px] bg-bgBrandLight1 rounded-[50%]">
             <div className="flex items-center justify-center w-[40px] h-[40px] bg-bgBrandLight2 rounded-[50%]">
               <EmptyCartIcon width={24} height={24} />
             </div>
           </div>
-          <p className="mb-[8px]">Кошик порожній</p>
-          <p className="mb-[24px]">Почніть додавати товари прямо зараз!</p>
+          <p className="mb-[8px] decoration-textPrimary font-normal mobile320:text-[24px] mobile320:leading-[28.8px] tablet1024:text-[28px] tablet1024:leading-[36.4px]">
+            Кошик порожній
+          </p>
+          <p className="mb-[24px] decoration-textTertiary font-normal mobile320:text-[15px] mobile320:leading-[21px] tablet1024:text-[16px] tablet1024:leading-[24px]">
+            Почніть додавати товари прямо зараз!
+          </p>
           <button
             type="submit"
-            className="mobile320:w-[258px] mobile375:w-[313px] desktop1440:w-[404px] h-[48px] font-medium text-[16px] leading-[22.4px] state-button text-textContrast"
+            className="state-button w-full tablet600:w-[285px] h-[48px] font-medium text-[16px] leading-[22.4px] text-textContrast"
             onClick={() => {
               if (typeof window !== 'undefined') {
                 resetLocalStorage();
