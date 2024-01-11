d31d32f67f36d98548d23ab3ad847298d7976480
Midland707 <rus14job@ukr.net>
2024-01-11 21:43:46 +0200

    divided cart to three parts

--- components/Modals/ModalCart.js
+++ components/Modals/ModalCart.js
@@ -5,10 +5,14 @@ import { useContext } from 'react';
 import { StatusContext } from '@/context/statusContext';
 import { CloseIcon } from 'universal-components-frontend/src/components/icons';
 import { useOnKeyDown } from '@/hooks/useOnClickOutside';
+import { useSelector } from 'react-redux';
+import { selectCart } from '../../redux/cart/cartSelector';
 
 const ModalCart = () => {
   const { resetLocalStorage, backToHomeUrl, showModalCart, setShowModalCart } =
     useContext(StatusContext);
+
+  const { totalAmount } = useSelector(selectCart);
 
   const onClose = () => {
     setShowModalCart(!showModalCart);
@@ -29,10 +33,10 @@ const ModalCart = () => {
         }}
         className="z-60 tablet1024:fixed tablet1024:top-1/2 tablet1024:left-1/2 tablet1024:transform tablet1024:-translate-x-1/2 tablet1024:-translate-y-1/2 bg-green-400
        flex flex-col h-[83vh] items-center tablet1024:border-[1px] tablet1024:border-borderDefault tablet1024:rounded-[8px]
-        p-[16px] mobile480:px-[24px] mobile480:py-[16px] tablet600:px-[24px] tablet600:py-[20px]
+        mobile480:px-[24px] tablet600:px-[24px] mobile480:py-[16px] tablet600:py-[20px]
         tablet1024:px-[48px] desktop1440:py-[40px] w-full tablet1024:w-[976px] tablet1024:h-[546px] desktop1440:h-[780px]"
       >
-        <div className="flex flex-col h-full w-full">
+        <div className="flex flex-col justify-between h-full w-full">
           <div className="flex items-center w-full mobile320:flex-row justify-between">
             <h5
               className="mr-[8px] decoration-textPrimary mobile320:font-medium mobile320:text-[18px] mobile320:leading-[25.2px]
@@ -52,32 +56,52 @@ const ModalCart = () => {
               <CloseIcon />
             </button>
           </div>
-          <div className="flex flex-col items-center w-full mt-auto mb-auto">
-            <div className="flex items-center justify-center mb-[8px] w-[59px] h-[59px] bg-bgBrandLight1 rounded-[50%]">
-              <div className="flex items-center justify-center w-[40px] h-[40px] bg-bgBrandLight2 rounded-[50%]">
-                <EmptyCartIcon width={24} height={24} />
+          {!totalAmount && (
+            <div className="flex flex-col items-center w-full mt-auto mb-auto">
+              <div className="flex items-center justify-center mb-[8px] w-[59px] h-[59px] bg-bgBrandLight1 rounded-[50%]">
+                <div className="flex items-center justify-center w-[40px] h-[40px] bg-bgBrandLight2 rounded-[50%]">
+                  <EmptyCartIcon width={24} height={24} />
+                </div>
               </div>
+              <p className="mb-[8px] decoration-textPrimary font-normal mobile320:text-[24px] mobile320:leading-[28.8px] tablet1024:text-[28px] tablet1024:leading-[36.4px]">
+                Кошик порожній
+              </p>
+              <p className="mb-[24px] decoration-textTertiary font-normal mobile320:text-[15px] mobile320:leading-[21px] tablet1024:text-[16px] tablet1024:leading-[24px]">
+                Почніть додавати товари прямо зараз!
+              </p>
+              <button
+                type="button"
+                className="state-button w-full tablet600:w-[285px] h-[48px] font-medium text-[16px] leading-[22.4px] text-textContrast"
+                onClick={() => {
+                  if (typeof window !== 'undefined') {
+                    resetLocalStorage();
+                    backToHomeUrl();
+                    onClose();
+                  }
+                }}
+              >
+                Перейти до каталогу
+              </button>
             </div>
-            <p className="mb-[8px] decoration-textPrimary font-normal mobile320:text-[24px] mobile320:leading-[28.8px] tablet1024:text-[28px] tablet1024:leading-[36.4px]">
-              Кошик порожній
-            </p>
-            <p className="mb-[24px] decoration-textTertiary font-normal mobile320:text-[15px] mobile320:leading-[21px] tablet1024:text-[16px] tablet1024:leading-[24px]">
-              Почніть додавати товари прямо зараз!
-            </p>
-            <button
-              type="submit"
-              className="state-button w-full tablet600:w-[285px] h-[48px] font-medium text-[16px] leading-[22.4px] text-textContrast"
-              onClick={() => {
-                if (typeof window !== 'undefined') {
-                  resetLocalStorage();
-                  backToHomeUrl();
-                  onClose();
-                }
-              }}
-            >
-              Перейти до каталогу
-            </button>
-          </div>
+          )}
+          {totalAmount && (
+            <div className="flex flex-col items-center gap-[10px] h-[116px] p-[16px] ">
+              <p className="font-medium text-[18px] leading-[25.2px] text-textPrimary">
+                Всього:<span>{totalAmount}</span> ₴
+              </p>
+              <button
+                type="button"
+                className="state-button w-full tablet600:w-[285px] h-[48px] font-medium text-[16px] leading-[22.4px] text-textContrast"
+                onClick={() => {
+                  if (typeof window !== 'undefined') {
+                    onClose();
+                  }
+                }}
+              >
+                Офомити замовлення
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
