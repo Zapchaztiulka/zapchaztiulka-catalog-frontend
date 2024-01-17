2f8c508bc2520f04cef943edcba89b4a484a4849
Midland707 <rus14job@ukr.net>
2024-01-17 21:27:07 +0200

    add content to cartCrdItem

--- components/Cart/CartCardItem.js
+++ components/Cart/CartCardItem.js
@@ -1,12 +1,30 @@
 'use client';
 import Counter from '../Buttons/Counter';
+import {
+  EmptyImageIcon,
+  TrashIcon,
+} from 'universal-components-frontend/src/components/icons';
 
-const CartCardItem = ({ name, id, photo, price, vendorCode}) => {
-
-    return (<div className="border border-borderDefault rounded-lg hover:shadow-md">
-        Я тут
-        {/* <Counter /> */}
-    </div>)
-}
+const CartCardItem = ({
+  id,
+  photo,
+  name,
+  vendorCode,
+  quantity,
+  totalPrice,
+}) => {
+  return (
+    <li
+      key={id}
+      className="border border-borderDefault rounded-lg hover:shadow-md"
+    >
+      <img src={photo[0]?.url} alt={photo[0]?.alt} />
+      <h5>{name}</h5>
+      <p>Артикул: {vendorCode}</p>
+      <Counter id={id} counterValue={quantity} />
+      <p>{totalPrice} ₴</p>
+    </li>
+  );
+};
 
 export default CartCardItem;
--- components/Cart/CartCardList.js
+++ components/Cart/CartCardList.js
@@ -4,11 +4,26 @@ import { useSelector } from 'react-redux';
 import { selectCart } from '../../redux/cart/cartSelector';
 
 const CartCardList = () => {
-    const { data } = useSelector(selectCart);
-    // const { photo, name, vendorCode, quantity, totalPrice } = data[0];
+  const { data } = useSelector(selectCart);
+  // const { photo, name, vendorCode, quantity, totalPrice } = data[0];
 
-    return (<CartCardItem />)
-    
-}
+  return (
+    <ul>
+      {data &&
+        data.map(({ id, photo, name, vendorCode, quantity, totalPrice }) => {
+          return (
+            <CartCardItem
+              id={id}
+              name={name}
+              photo={photo}
+              vendorCode={vendorCode}
+              quantity={quantity}
+              totalPrice={totalPrice}
+            />
+          );
+        })}
+    </ul>
+  );
+};
 
 export default CartCardList;
