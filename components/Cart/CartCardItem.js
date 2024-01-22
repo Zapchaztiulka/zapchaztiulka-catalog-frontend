4e9359dd8116b962212486e24086b9a07973cdb8
Midland707 <rus14job@ukr.net>
2024-01-22 23:06:46 +0200

    add styles of cart for tablet768

--- components/Cart/CartCardItem.js
+++ components/Cart/CartCardItem.js
@@ -19,36 +19,42 @@ const CartCardItem = ({
   const [isOpenDeleteModal, setIsOpenDeleteModal] = useState(false);
   return (
     <li
-      className="flex flex-col h-[150px] py-[16px] px-[4px] gap-[16px] tablet600:gap-[4px]
+      className="flex flex-col h-full py-[16px] tablet600:py-[12px] px-[4px] mobile375:px-[8px] tablet600:px-[12px] 
+      gap-[16px] mobile375:gap-[12px] mobile480:gap-[8px] tablet600:gap-[4px]
       border border-borderDefault rounded-lg hover:shadow-md"
     >
-      <div className="flex items-center justify-between w-full gap-[4px] h-[62px] tablet600:h-[72px]">
-        <div
-          className="flex items-center justify-center min-w-[62px] mobile375:min-w-[76px] tablet600:min-w-[108px]
-        h-[62px] tablet600:h-[72px]"
-        >
-          {photo[0] ? (
-            <Image
-              src={photo[0]?.url}
-              alt={photo[0]?.alt}
-              width="62"
-              height="62"
-              className="object-scale-down h-[62px] tablet600:h-[72px] tablet600:w-[72px]"
-            />
-          ) : (
-            <EmptyImageIcon size="50px" />
-          )}
+      <div className="flex items-center justify-between w-full tablet768:gap-[20px] h-[62px] tablet600:h-[72px]">
+        <div className="flex items-center gap-[4px]">
+          <div className="flex items-center justify-center min-w-[62px] mobile375:min-w-[76px] tablet600:min-w-[108px] h-[62px] tablet600:h-[72px]">
+            {photo[0] ? (
+              <Image
+                src={photo[0]?.url}
+                alt={photo[0]?.alt}
+                width="62"
+                height="62"
+                className="object-scale-down h-[62px] tablet600:h-[72px] tablet600:w-[72px]"
+              />
+            ) : (
+              <EmptyImageIcon size="50px" />
+            )}
+          </div>
+          <div className="flex flex-col w-fit mr-auto">
+            <h5 className="font-medium text-[14px] leading-[19.6px] decoration-textPrimary">
+              {name}
+            </h5>
+            <p className="font-normal text-[12px] leading-[18px] decoration-textTertiary">
+              Артикул: {vendorCode}
+            </p>
+          </div>
         </div>
-        <div className="flex flex-col w-fit mr-auto">
-          <h5 className="font-medium text-[14px] leading-[19.6px] decoration-textPrimary">
-            {name}
-          </h5>
-          <p className="font-normal text-[12px] leading-[18px] decoration-textTertiary">
-            Артикул: {vendorCode}
-          </p>
+        <div className="hidden tablet768:block ml-auto border border-borderDefault rounded-lg hover:shadow-md">
+          <Counter id={id} counterValue={quantity} />
         </div>
+        <p className="hidden tablet768:block font-normal text-[15px] leading-[21px] decoration-textPrimary min-w-[124px] text-right">
+          {totalPrice} ₴
+        </p>
         <button
-          className="flex items-center justify-center min-w-[42px] h-[42px] mobile375:min-w-[44px] mobile375:h-[44px]"
+          className="flex items-center justify-center min-w-[44px] h-[44px]"
           type="button"
           onClick={() => {
             setIsOpenDeleteModal(true);
@@ -57,12 +63,12 @@ const CartCardItem = ({
           <TrashIcon color="#888D92" />
         </button>
       </div>
-      <div className="flex justify-between items-center w-full h-[40px] px-[4px] mobile375:pl-[80px] tablet600:pl-[112px]">
+      <div className="tablet768:hidden flex justify-between items-center w-full h-[40px] px-[4px] mobile375:pl-[80px] tablet600:pl-[112px]">
         <div className="border border-borderDefault rounded-lg hover:shadow-md">
           <Counter id={id} counterValue={quantity} />
         </div>
         <p
-          className="font-medium text-[16px] leading-[22.4px] decoration-textPrimary
+          className="font-medium mobile480:font-normal text-[16px] leading-[22.4px] decoration-textPrimary
         mobile375:font-normal mobile375:text-[14px] mobile375:leading-[19.6px] 
         mobile480:text-[15px] mobile480:leading-[21px]"
         >
